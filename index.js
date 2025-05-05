5d7587ce73001f765b0fe2c3201b9a50e90debf3
NotAthus <48191311+athusderia@users.noreply.github.com>
2025-05-04 22:15:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+//Middleware es una acción intermedia que sirve para hacer algo especifico
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express();
 
 //Middleware morgan
-app.use(morgan('tiny'))
+app.use(morgan('dev'))
+//Es un middleware
+app.use(express.json())
 
 app.get('/profile', (req, res)=>{
     res.send('Profile Page')
